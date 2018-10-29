d95fe20cc478212d8096cda2a39bd0cd57c796c5
Kevin Harrington <mad.hephaestus@gmail.com>
2018-10-29 13:02:54 -0400

    adding class

---
+++ LabCodeRepoSetup/src/main/java/edu/wpi/rbe/LabCodeRepoSetupMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.wpi.rbe;
+
+/**
+ * @author hephaestus
+ *
+ */
+public class LabCodeRepoSetupMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
